526a9a0a11d609067b011bdfa89d47e0675159ef
Matt Thomson <mthomson.lee@gmail.com>
2018-07-28 11:51:57 -0400

    Calculates all square midpoints

--- src/grid.js
+++ src/grid.js
@@ -1,13 +1,46 @@
 const x = 0;
 const y = 1;
 
-const getSize = (n) => Math.pow( n , 2 ) + 1;
-const createGrid = (size) => Array.apply(null, { length: size } );
+const getSize = (n) => Math.pow( 2, n ) + 1;
+const createGrid = (size) => Array.apply(null, { length: size * size } );
 
 const indexToPos = (index, size) => [ index % size, Math.floor( index / size ) ];
 const posToIndex = (pos, size) => pos[y] * size + pos[x];
+
+const squareMidpoints = (size) => {
+  const midpoint = ( size * size - 1 ) / 2;
+  const step = Math.log2( size - 1 ) - 1;
+  const results = [midpoint];
+
+  if ( step > 0 ) {
+    results.push(...nextSquareMidpoints(midpoint, size, step));
+  }
+
+  return results;
+};
+
+const nextSquareMidpoints = (index, size, step) => {
+  const results = [];
+  results.push(index - size * step - step);
+  results.push(index - size * step + step);
+  results.push(index + size * step - step);
+  results.push(index + size * step + step);
+
+  if ( step > 1 ) {
+    const subResults = [];
+
+    results.forEach((resultIndex) => {
+      subResults.push(...nextSquareMidpoints(resultIndex, size, step - 1));
+    });
+
+    results.push(...subResults);
+  }
+
+  return results;
+};
 
 module.exports.createGrid = createGrid;
 module.exports.getSize = getSize;
 module.exports.indexToPos = indexToPos;
 module.exports.posToIndex = posToIndex;
+module.exports.squareMidpoints = squareMidpoints;
--- test/grid.js
+++ test/grid.js
@@ -5,18 +5,22 @@ const getSize = require('../src/grid').getSize;
 const indexToPos = require('../src/grid').indexToPos;
 const posToIndex = require('../src/grid').posToIndex;
 
+const squareMidpoints = require('../src/grid').squareMidpoints;
+
 describe('Grid Utilities', () => {
   it('should return (2^n)+1', () => {
     expect(getSize(2)).to.equal(5);
-    expect(getSize(3)).to.equal(10);
+    expect(getSize(3)).to.equal(9);
     expect(getSize(4)).to.equal(17);
+    expect(getSize(5)).to.equal(33);
   });
 
   it('should return an array of proper size with undefined values', () => {
     let grid = createGrid(5);
-    expect(grid.length).to.equal(5);
+    expect(grid.length).to.equal(25);
     expect(grid[0]).to.equal(undefined);
-    expect(grid[4]).to.equal(undefined);
+    expect(grid[13]).to.equal(undefined);
+    expect(grid[24]).to.equal(undefined);
   });
 
   it('should convert index to position', () => {
@@ -33,5 +37,24 @@ describe('Grid Utilities', () => {
     expect(posToIndex([0, 1], 3)).to.equal(3);
     expect(posToIndex([2, 1], 3)).to.equal(5);
     expect(posToIndex([2, 2], 3)).to.equal(8);
+  });
+});
+
+describe('Square Midpoint Index Generation', () => {
+  it('n=1 grid should generate midpoints correctly', () => {
+    expect(squareMidpoints(getSize(1))).to.deep.equal([4]);
+  });
+  it('n=2 grid should generate midpoints correctly', () => {
+    expect(squareMidpoints(getSize(2))).to.deep.equal([12, 6, 8, 16, 18]);
+  });
+  it('n=3 grid should generate midpoints correctly', () => {
+    expect(squareMidpoints(getSize(3))).to.deep.equal([
+      40,
+        20, 24, 56, 60,
+          10, 12, 28, 30,
+          14, 16, 32, 34,
+          46, 48, 64, 66,
+          50, 52, 68, 70
+    ]);
   });
 });
