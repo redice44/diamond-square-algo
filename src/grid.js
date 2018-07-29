40e724eb69f8c7c237a57a9c9e7a73e0bd4f64f9
Matt Thomson <mthomson.lee@gmail.com>
2018-07-29 01:59:16 -0400

    Calculates square midpoints better

--- src/grid.js
+++ src/grid.js
@@ -1,39 +1,30 @@
-const x = 0;
-const y = 1;
-
 const getSize = (n) => Math.pow( 2, n ) + 1;
 const createGrid = (size) => Array.apply(null, { length: size * size } );
-
 const indexToPos = (index, size) => [ index % size, Math.floor( index / size ) ];
-const posToIndex = (pos, size) => pos[y] * size + pos[x];
+const posToIndex = (pos, size) => pos[1] * size + pos[0];
 
-const squareMidpoints = (size) => {
-  const midpoint = ( size * size - 1 ) / 2;
-  const step = Math.log2( size - 1 ) - 1;
-  const results = [midpoint];
+const getAllSquares = (n) => {
+  const results = [];
 
-  if ( step > 0 ) {
-    results.push(...nextSquareMidpoints(midpoint, size, step));
+  for ( let i = 0; i < n; i++ ) {
+    results.push(...getSquares(n, i));
   }
 
   return results;
 };
 
-const nextSquareMidpoints = (index, size, step) => {
+const getSquares = (n, step) => {
+  const size = Math.pow( 2, n ) + 1;
+  const jumpBase = Math.pow( 2, n - step );
+  const offsetBase = Math.pow( 2, n - 1 - step );
+  const offset = offsetBase * size + offsetBase;
+  const loop = Math.pow( 2, step );
   const results = [];
-  results.push(index - size * step - step);
-  results.push(index - size * step + step);
-  results.push(index + size * step - step);
-  results.push(index + size * step + step);
 
-  if ( step > 1 ) {
-    const subResults = [];
-
-    results.forEach((resultIndex) => {
-      subResults.push(...nextSquareMidpoints(resultIndex, size, step - 1));
-    });
-
-    results.push(...subResults);
+  for (let row = 0; row < loop; row++) {
+    for (let col = 0; col < loop; col++) {
+      results.push( offset + row * size * jumpBase + col * jumpBase );
+    }
   }
 
   return results;
@@ -43,4 +34,6 @@ module.exports.createGrid = createGrid;
 module.exports.getSize = getSize;
 module.exports.indexToPos = indexToPos;
 module.exports.posToIndex = posToIndex;
-module.exports.squareMidpoints = squareMidpoints;
+
+module.exports.getAllSquares = getAllSquares;
+module.exports.getSquares = getSquares;
--- test/grid.js
+++ test/grid.js
@@ -5,7 +5,7 @@ const getSize = require('../src/grid').getSize;
 const indexToPos = require('../src/grid').indexToPos;
 const posToIndex = require('../src/grid').posToIndex;
 
-const squareMidpoints = require('../src/grid').squareMidpoints;
+const getSquares = require('../src/grid').getSquares;
 
 describe('Grid Utilities', () => {
   it('should return (2^n)+1', () => {
@@ -42,19 +42,57 @@ describe('Grid Utilities', () => {
 
 describe('Square Midpoint Index Generation', () => {
   it('n=1 grid should generate midpoints correctly', () => {
-    expect(squareMidpoints(getSize(1))).to.deep.equal([4]);
+    expect(getSquares(1, 0)).to.deep.equal([
+      4
+    ]);
   });
   it('n=2 grid should generate midpoints correctly', () => {
-    expect(squareMidpoints(getSize(2))).to.deep.equal([12, 6, 8, 16, 18]);
+    expect(getSquares(2, 0)).to.deep.equal([
+      12
+    ]);
+    expect(getSquares(2, 1)).to.deep.equal([
+      6,  8,
+      16, 18
+    ]);
   });
   it('n=3 grid should generate midpoints correctly', () => {
-    expect(squareMidpoints(getSize(3))).to.deep.equal([
-      40,
-        20, 24, 56, 60,
-          10, 12, 28, 30,
-          14, 16, 32, 34,
-          46, 48, 64, 66,
-          50, 52, 68, 70
+    expect(getSquares(3, 0)).to.deep.equal([
+      40
+    ]);
+    expect(getSquares(3, 1)).to.deep.equal([
+      20, 24,
+      56, 60
+    ]);
+    expect(getSquares(3, 2)).to.deep.equal([
+      10, 12, 14, 16,
+      28, 30, 32, 34,
+      46, 48, 50, 52,
+      64, 66, 68, 70
+    ]);
+  });
+  it('n=4 grid should generate midpoints correctly', () => {
+    expect(getSquares(4, 0)).to.deep.equal([
+      144
+    ]);
+    expect(getSquares(4, 1)).to.deep.equal([
+      72,  80,
+      208, 216
+    ]);
+    expect(getSquares(4, 2)).to.deep.equal([
+      36,  40,  44,  48,
+      104, 108, 112, 116,
+      172, 176, 180, 184,
+      240, 244, 248, 252
+    ]);
+    expect(getSquares(4, 3)).to.deep.equal([
+      18,  20,  22,  24,  26,  28,  30,  32,
+      52,  54,  56,  58,  60,  62,  64,  66,
+      86,  88,  90,  92,  94,  96,  98,  100,
+      120, 122, 124, 126, 128, 130, 132, 134,
+      154, 156, 158, 160, 162, 164, 166, 168,
+      188, 190, 192, 194, 196, 198, 200, 202,
+      222, 224, 226, 228, 230, 232, 234, 236,
+      256, 258, 260, 262, 264, 266, 268, 270
     ]);
   });
 });
